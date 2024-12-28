eb83f174f7b712a86ce849fa9c727ad82e554d16
IANNXANG <yx17180324@gmail.com>
2024-12-28 17:08:43 +0800

    , encoding='utf-8'

--- resnet50.py
+++ resnet50.py
@@ -7,7 +7,7 @@ import logging
 def main_resnet50():
     # 配置日志记录
     logging.basicConfig(filename='accuracy_resnet50.log', level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s')
+                        format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8')
 
     # 加载预训练的ResNet50模型
     model = models.resnet50(pretrained=True)
